f3fd1b129139662c070eeec7d50dbdf6f9733919
TheScarastic <warabhishek@gmail.com>
2021-10-30 08:30:42 +0200

    NX659J: fingerprint: Update HAL to v2.3 to support UDFPS
    Change-Id: I5d33380a238ad33b3ca79c5ffe0b148cdfa57a8e
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- *               2022 The LineageOS Project
+ *               2022-2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.NX659J"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.NX659J"
 
-#include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 #include <android/hardware/biometrics/fingerprint/2.1/types.h>
+#include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
 #include <android/log.h>
 #include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
@@ -17,8 +17,8 @@
 using android::sp;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
-using android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
-using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
 int main() {
     android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
