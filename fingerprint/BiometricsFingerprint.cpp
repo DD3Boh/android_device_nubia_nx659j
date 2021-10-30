dc75bb83ce0dbb22c3a4afdaa94fa952fdd36981
TheScarastic <warabhishek@gmail.com>
2021-10-30 08:30:42 +0200

    NX659J: fingerprint: Update HAL to v2.3 to support UDFPS
    Change-Id: I5d33380a238ad33b3ca79c5ffe0b148cdfa57a8e
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- *               2022 The LineageOS Project
+ *               2022-2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.NX659J"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.NX659J"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.NX659J"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.3-service.NX659J"
 
 #include <hardware/hw_auth_token.h>
 
@@ -20,7 +20,7 @@ namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
@@ -50,6 +50,18 @@ BiometricsFingerprint::~BiometricsFingerprint() {
         return;
     }
     mDevice = nullptr;
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
 }
 
 Return<RequestStatus> BiometricsFingerprint::ErrorFilter(int32_t error) {
@@ -147,7 +159,7 @@ Return<uint64_t> BiometricsFingerprint::setNotify(
         const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
     std::lock_guard<std::mutex> lock(mClientCallbackMutex);
     mClientCallback = clientCallback;
-    // This is here because HAL 2.1 doesn't have a way to propagate a
+    // This is here because HAL 2.3 doesn't have a way to propagate a
     // unique token for its driver. Subsequent versions should send a unique
     // token for each call to setNotify(). This is fine as long as there's only
     // one fingerprint device on the platform.
@@ -236,7 +248,7 @@ fingerprint_device_t* BiometricsFingerprint::openHal() {
     }
 
     if (kVersion != device->version) {
-        // enforce version on new devices because of HIDL@2.1 translation layer
+        // enforce version on new devices because of HIDL@2.3 translation layer
         ALOGE("Wrong fp version. Expected %d, got %d", kVersion, device->version);
         return nullptr;
     }
@@ -312,6 +324,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t* msg) {
                                                msg->data.authenticated.finger.gid, token)
                              .isOk()) {
                     ALOGE("failed to invoke fingerprint onAuthenticated callback");
+                    getInstance()->onFingerUp();
                 }
             } else {
                 // Not a recognized fingerprint
@@ -339,7 +352,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t* msg) {
 }
 
 }  // namespace implementation
-}  // namespace V2_1
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware
