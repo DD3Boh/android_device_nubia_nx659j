86be92f0f3fa1bf8265419cd934f382656ec8afa
Davide Garberi <dade.garberi@gmail.com>
2023-06-10 01:56:30 +0200

    DNM: Disable nubia parts
    Change-Id: Id64f179fd1320d9dbf7d49e46d5ded27d5be890d

--- device.mk
+++ device.mk
@@ -285,8 +285,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Parts
-PRODUCT_PACKAGES += \
-    NubiaParts
+#PRODUCT_PACKAGES += \
+#    NubiaParts
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
