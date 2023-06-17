f14899c5c2f6420bd11b90898147608704aeef3c
Davide Garberi <dade.garberi@gmail.com>
2023-06-17 18:42:27 +0200

    NX659J: Build custom fingerprint 2.3 hal
    Change-Id: I84a8bcf6f7b81bc02e32fdc145dc200076f1db8b

--- device.mk
+++ device.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.3-service.NX659J
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
